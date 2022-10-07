8c52c947ca975e209905844bbed24cb57fd37c15
JustWaveThings <justwavethings@proton.me>
2022-10-07 17:41:00 -0400

    Working example of getter and setter in factory function

--- fft.js
+++ fft.js
@@ -1,6 +1,27 @@
-const Player = (name, marker) => {
-	const getName = () => prompt('What is your name?');
-	return { name, marker, getName };
+///
+
+const Player = () => {
+	let name = 'Alex';
+
+	const setName = (newName) => {
+		name = newName;
+	};
+	const getName = () => name;
+	return { getName, setName };
 };
 
-const player1 = Player(getName(), 'X');
+const player1 = Player();
+
+console.log(player1);
+player1.name = 'Tabitha';
+console.log(player1);
+player1.setName();
+console.log(player1);
+/* console.log(player1);
+player1.name = 'toby';
+console.log(player1);
+player1.name = 'jeremy';
+console.log(player1);
+player1.set();
+console.log(player1);
+ */
